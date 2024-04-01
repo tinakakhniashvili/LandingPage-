6f005e4bafd21708c4be914bd9909356364b0944
tinakakhniashvili <hosktina@gmail.com>
2024-04-01 19:09:51 +0400

    3rd Quiz

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleRegistrationForm() {
+    var form = document.getElementById("registrationForm");
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  }
